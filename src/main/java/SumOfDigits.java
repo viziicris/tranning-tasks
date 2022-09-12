bde022bc672abda286cf4ea4b918c281ae2a759f
viziicris <vizii.cristina@gmail.com>
2022-09-12 22:00:19 +0300

    test pull request changes

--- src/main/java/SumOfDigits.java
+++ src/main/java/SumOfDigits.java
@@ -1,11 +1,9 @@
 public class SumOfDigits {
-
     public static void main(String[] args) {
         int number1 = 22;
         int sum=0;
         String stringnumber = Integer.toString(number1);
         for (int i = 0; i < stringnumber.length(); i++) {
-
             sum=sum+(stringnumber.charAt(i)-48);
         }
         System.out.println(sum);

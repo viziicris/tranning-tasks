3af79f1eef412b732dc7780398b0db17311fad83
viziicris <vizii.cristina@gmail.com>
2022-09-14 18:56:46 +0300

    Middle word

--- src/main/java/MiddleWord.java
+++ src/main/java/MiddleWord.java
@@ -1,9 +1,10 @@
 public class MiddleWord {
     public static void main(String[] args) {
-        String string = "today is my best day";
+        String string = "today is my best day because I met u";
 
         String[] bits = string.split(" ");
-        String middleWord = bits[bits.length-3];
+        int nr = bits.length/2+1;
+        String middleWord = bits[bits.length-nr];
         System.out.println(middleWord);
     }
 }

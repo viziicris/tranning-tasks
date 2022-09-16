b02a5f7a7dbd241e1905030c87d4a1cb84770652
viziicris <vizii.cristina@gmail.com>
2022-09-16 21:01:38 +0300

    true or false

--- src/main/java/MiddleWord.java
+++ src/main/java/MiddleWord.java
@@ -1,7 +1,6 @@
 public class MiddleWord {
     public static void main(String[] args) {
         String string = "today is my best day because I met u";
-
         String[] bits = string.split(" ");
         int nr = bits.length/2+1;
         String middleWord = bits[bits.length-nr];
---
+++ src/main/java/TrueFalse.java
@@ -0,0 +1,8 @@
+public class TrueFalse {
+    public static void main(String[] args) {
+        String s1="hello for this world";
+        String s2="hello";
+        String[] bits = s1.split(" ");
+        System.out.println(bits[0].equals(s2));
+    }
+}
